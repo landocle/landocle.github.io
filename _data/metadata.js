23a07b792a21a5e9aaeeb097610d4e367ea1c9b4
= <86326695+landocle@users.noreply.github.com>
2023-07-28 19:25:43 -0400

    Add basic blog

---
+++ _data/metadata.js
@@ -0,0 +1,9 @@
+module.exports = {
+	title: "landocle",
+	url: "https://landocle.com/",
+	language: "en",
+	description: "landocle's Blog",
+	author: {
+		name: "landocle"
+	}
+}
---
+++ content/blog/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: Hello, world!
+description: This is the initial post to get this blog started.
+date: 2023-07-28
+---
+Hello, world!
